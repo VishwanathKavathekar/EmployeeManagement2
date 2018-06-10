d5d7dbe939de7c35facb1f6ef89c5f8708cbd6b7
user <user@PAVILION-10>
2018-06-10 12:29:37 +0530

    Employee Management System

---
+++ EmployeeManagement2/src/employeemanagement2/addemp.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employeemanagement2;
+
+/**
+ *
+ * @author user
+ */
+public class addemp {
+   
+    int age;
+    String name,address;
+    double salary;
+    
+    
+   void account()
+    {
+       this.age=0;
+       this.name=null;
+       this.address=null;
+       this.salary=0;
+    }
+
+}
+
+
