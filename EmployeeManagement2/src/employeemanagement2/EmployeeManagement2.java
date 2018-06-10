4fe1322cf5ef5e5b67dfaa4fce1489ab2880621b
user <user@PAVILION-10>
2018-06-10 16:49:02 +0530

    Employee Management System

--- EmployeeManagement2/src/employeemanagement2/EmployeeManagement2.java
+++ EmployeeManagement2/src/employeemanagement2/EmployeeManagement2.java
@@ -11,7 +11,8 @@ import java.util.ArrayList;
 
 /**
  *
- * @author user
+ * @author Anusha
+ * @author Vishwanath
  */
 
 class employee
@@ -33,35 +34,6 @@ class employee
       
     }
     
-    public void update_emp(int id)
-    {
-        addemp a = new addemp(); 
-        if(id<=emp_id ) 
-         {
-                System.out.println("Name:"+acc.get(id).name);
-                System.out.println("Age:"+acc.get(id).age);
-                System.out.println("Address:"+acc.get(id).address);
-                System.out.println("Salary:"+acc.get(id).salary + "\n");
-          }
-        //acc.remove(id);
-        System.out.println("Enter new details\n");
-        System.out.println("Enter the employee name:");
-        String name=input.next();
-        a.name=name;
-        System.out.println("Enter age of the employee:");
-        int age=input.nextInt();
-        a.age=age;
-        System.out.println("Enter Salary of the employee");
-        double salary = input.nextDouble();
-        a.salary=salary;
-        System.out.println("Enter address of the emplyee");
-        String address=input.next();
-        a.address=address;
-        acc.add(a);
-        System.out.println("The employee ID is\n" + id );         
-    }
-    
-    
 }
 public class EmployeeManagement2 {
 
@@ -69,7 +41,7 @@ public class EmployeeManagement2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        {
+        
     employee e = new employee();
     Scanner input = new Scanner(System.in);
     int emp_age,emp_id;
@@ -104,7 +76,8 @@ public class EmployeeManagement2 {
             
         case 3: System.out.println("Enter employee ID: ");
                 emp_id=input.nextInt();
-                e.update_emp(emp_id);
+                update up = new update();
+                up.update(emp_id);
             break;
             
         case 4: System.out.println("Enter employee ID: ");
@@ -113,16 +86,15 @@ public class EmployeeManagement2 {
                 dis.display(emp_id);
             break;
         
-        case 5:System.exit(0);
+        case 5: System.exit(0);
             break;
             
         default : System.out.println("Enter choice from 1-4");
             break;
     }
         
-    }while(choice!=5);
+   }while(choice!=5);
     
-    }
-    }
+ }
     
 }
